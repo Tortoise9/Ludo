c9cf5737531f64bb11c5b91149573fcb4e1ce9d7
sushil pandey <sushilpandeyy9@gmail.com>
2023-01-30 19:57:04 +0545

    Ludo Game

---
+++ ludo.js
@@ -0,0 +1,8 @@
+
+let count;
+    function call(){
+        let mars=document.getElementById("homew");
+        let target = Math.floor(Math.random()*6)+1;
+        console.log(target)
+        mars.textContent=target;
+    }
